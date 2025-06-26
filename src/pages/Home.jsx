1bba561d40de615bf653d96efa2261a6bf5ea7a9
Simon Muriu <muriusimon6@gmail.com>
2025-06-26 13:29:13 +0300

    Fix import path for Button and Card to include .jsx extension

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card } from '../components/ui/Card';
-import { Button } from '../components/ui/Button';
+import { Card } from '../components/ui/Card.jsx';
+import { Button } from '../components/ui/Button.jsx';
 
 const Home = () => {
   return (
